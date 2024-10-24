001dddf27469336384a8cb25b2dcd7cb647bb0dc
swornadt <tuladharsworna@gmail.com>
2024-10-24 17:27:10 +0545

    added url to const

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_ENDPOINT = "api/v1/user";
-export const MSG_API_ENDPOINT = "api/v1/message";
-export const BASE_API_ENDPOINT = "api/v1";
+export const USER_API_ENDPOINT = "https://messagingapp-tibe.onrender.com/api/v1/user";
+export const MSG_API_ENDPOINT = "https://messagingapp-tibe.onrender.com/api/v1/message";
+export const BASE_API_ENDPOINT = "https://messagingapp-tibe.onrender.com/api/v1";
