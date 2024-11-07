b858f145ed1351544d55968c5e9268174f19767d
swornadt <tuladharsworna@gmail.com>
2024-11-07 09:26:54 +0545

    db debuging

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,12 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const databaseConnection = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    mongoose.set('debug', true);
+    console.log("Connecting to MongoDB at:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.log("DB connected");
     }).catch((error) => {
         console.log("Not connected!!!!!!!!!!!!!!!!",error);
